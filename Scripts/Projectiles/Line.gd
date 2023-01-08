06c6f76b7919ed0fc5a606f41bfc10ad59eae754
Lock-The-Door <Lock-The-Door@outlook.com>
2023-01-07 20:40:22 -0500

    Use inheritance for projectiles

--- Scripts/Projectiles/Line.gd
+++ Scripts/Projectiles/Line.gd
@@ -1,44 +1,6 @@
-extends Node2D
+extends "res://Scripts/Projectile.gd"
 
-onready var _player = get_node("/root/Game/Player")
-onready var _collisionNode = get_node("Area2D")
-
-var _projectileSpeed
-var _projectileLifespan
-var _projectileDamage
-var _isEnemyProjectile
-
-func init(angle, speed, lifespan, damage, isEnemyProjectile = false):
-	_projectileSpeed = speed
-	self.rotation = angle
-	_projectileLifespan = lifespan
-	_projectileDamage = damage
-	_isEnemyProjectile = isEnemyProjectile
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# move
 	var rotation = Vector2(1, 0).rotated(self.rotation)
 	self.position += rotation * delta * _projectileSpeed
-
-	# lifespan
-	_projectileLifespan -= delta
-	if _projectileLifespan <= 0:
-		self.queue_free()
-		return
-		
-	# collision
-	var colliding = _collisionNode.get_overlapping_areas()
-	# traverse parents until we find a node containing the player/enemy group
-	for area in colliding:
-		var parent = area.get_parent()
-		while parent != null:
-			if parent.is_in_group("player") and _isEnemyProjectile:
-				parent.damage(_projectileDamage)
-				self.queue_free()
-				return
-			elif parent.is_in_group("enemy") and not _isEnemyProjectile:
-				parent.damage(_projectileDamage)
-				self.queue_free()
-				return
-			parent = parent.get_parent()
