0a1ebad80f3d8fed04163d7e8c470089f698821e
Lock-The-Door <Lock-The-Door@outlook.com>
2023-01-16 18:32:01 -0500

    load the shop item image properly

--- Scripts/Upgrade Menu/Shop Item.gd
+++ Scripts/Upgrade Menu/Shop Item.gd
@@ -3,6 +3,4 @@ extends AspectRatioContainer
 func init(itemData):
 	get_node("Button/Container/Text/Name").text = itemData.name
 	get_node("Button/Container/Text/Price").text = str(itemData.price) + " snow"
-	var image = Image.new()
-	image.load(itemData.image)
-	get_node("Button/Container/Image").texture.create_from_image(image)
+	get_node("Button/Container/Image").texture = load(itemData.image)
